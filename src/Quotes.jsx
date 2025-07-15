4410bbb1069c4f98ad3e6167d6bd3aa53ce4c41f
Muhamad Ikhsan <muhamadikhsan.net@gmail.com>
2025-07-15 21:37:00 +0700

    Update Quotes.jsx

--- src/Quotes.jsx
+++ src/Quotes.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import "./Quotes.css";
+import "./quotes.css";
 
 const fullText = `Selamat ulang tahun ke-17! 🥳🥳 Selamat datang di "Sweet Seventeen" yang katanya manis. Yah, semoga aja benaran manis. Menurut saya, sekarang adalah 'golden hour'-nya kehidupan, di mana ada banyak hal yang bisa dicoba tanpa takut gagal. Ada yang bilang, "no risk, no Ferrari", anggep aja bahwa di balik risiko yang besar, terdapat keberuntungan yang sama besarnya. Anggap semua tantangan sebagai sebuah peluang buat belajar banyak hal baru dan jadi lebih tangguh untuk terus mengejar cita-cita. Di umur ke-17, yang merupakan sebuah transisi dari masa remaja menuju kedewasaan, semoga kamu menjadi pribadi yang lebih baik (walaupun sekarang sudah baik banget 🙄🙄), berani nyobain banyak hal (meski sekarang dah bisa banyak hal 👌), bijaksana dalam setiap keputusan yang diambil, serta menjadi pribadi yang lebih terbuka kepada diri sendiri maupun orang lain. Saya juga berharap semua hari-hari yang kamu jalani dipenuhi kebahagiaan, kemudahan, dan kesuksesan.`;
 
